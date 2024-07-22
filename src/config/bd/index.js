8a159c27c378cb9e71dd8bfd63ef79fe3887604f
HAIDND <dangngochai280603@gmail.com>
2024-07-23 01:21:56 +0700

    new connect

--- src/config/bd/index.js
+++ src/config/bd/index.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 //mongodb+srv://dangngochai280306:VIYIO9sCThpHz8jj@cluster0.vq6y6wc.mongodb.net/
-
+//mongodb+srv://xiao98581:qfIKGGalbMiyIcOC@xiaocluster0.yeiejsh.mongodb.net/?retryWrites=true&w=majority&appName=xiaoCluster0
 async function connect() {
     try {
         // await mongoose.connect('mongodb://localhost:27017/web_nodejs_db');
-        await mongoose.connect('mongodb+srv://dangngochai280306:VIYIO9sCThpHz8jj@cluster0.vq6y6wc.mongodb.net/web_nodejs_db');
+        await mongoose.connect('mongodb+srv://xiao98581:qfIKGGalbMiyIcOC@xiaocluster0.yeiejsh.mongodb.net/?retryWrites=true&w=majority&appName=xiaoCluster0/web_nodejs_db');
         //     , {
         //     useNewUrlParser: true,
         //     useUnifiedTopology: true
